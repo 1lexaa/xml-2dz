ea7340e99b81459d913801d4240ca4859da83134
gh0sty <nice140@icloud.com>
2022-03-31 02:28:08 +0300

    commit initial

---
+++ xml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace IO
+{
+    public class Item
+    {
+        public string Tittle { get; set; }
+        public float Price { get; set; }
+        public float Sale { get; set; }
+
+    }
+
+    internal class XML
+    {
+
+        public static void Main()
+        {
+            Random random = new Random();
+
+            Item[] items = new Item[5];
+            items[0] = new Item() { Tittle = "title", Price = random.Next(1000), Sale = (float)random.NextDouble() * 10 };
+            items[1] = new Item() { Tittle = "title", Price = random.Next(1000), Sale = (float)random.NextDouble() * 10 };
+            items[2] = new Item() { Tittle = "title", Price = random.Next(1000), Sale = (float)random.NextDouble() * 10 };
+            items[3] = new Item() { Tittle = "title", Price = random.Next(1000), Sale = (float)random.NextDouble() * 10 };
+            items[4] = new Item() { Tittle = "title", Price = random.Next(1000), Sale = (float)random.NextDouble() * 10 };
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Item[]));
+
+            using (var sr = new StreamWriter("xml.xml"))
+            {
+                serializer.Serialize(sr, items);
+            }
+
+        }
+
+
+
+    }
+}
---
+++ xmlDocument.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace IO
+{
+    internal class XmlDocumentDemo
+    {
+        public static void Main()
+        {
+            XmlDoc();
+        }
+
+        public static void XmlDoc()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+
+            try
+            {
+                xmlDoc.Load("Products.xml");
+                Console.WriteLine("Load");
+            }
+            catch
+            {
+                Console.WriteLine("Load err");
+            }
+
+            RecursivePrint(xmlDoc.DocumentElement.ChildNodes);
+
+            //Console.WriteLine();
+            //CreateNewProduct(xmlDoc);
+
+            //PrintXml(xmlDoc.DocumentElement.ChildNodes);
+        }
+
+        public static void PrintXml(XmlNodeList chidNodes)
+        {
+            if (chidNodes.Count.Equals(0))
+            {
+                Console.WriteLine("Xml is empty");
+            }
+            else
+            {
+                foreach (XmlNode child in chidNodes)
+                {
+                    Console.Write($"Title:{child["Title"].InnerText} ");
+                    Console.Write($"Price:{Convert.ToSingle(child["Price"].InnerText)} ");
+                    Console.WriteLine($"Discount:{Convert.ToSingle(child["Discount"].InnerText)}");
+                }
+            }
+        }
+
+        public static void RecursivePrint(XmlNodeList chidNodes)
+        {
+            for (int i = 0; i < chidNodes.Count; i++)
+            {
+                for (int j = 0; j < chidNodes[i].ChildNodes.Count; j++)
+                {
+                    if (chidNodes[i].ChildNodes[j].HasChildNodes)
+                    {
+                        Console.Write($"{chidNodes[i].ChildNodes[j].Name}:");
+                        Console.WriteLine(chidNodes[i].ChildNodes[j].InnerText);
+                    }
+                }
+
+                if (chidNodes[i].HasChildNodes)
+                {
+                    RecursivePrint(chidNodes[i].ChildNodes);
+                }
+            }
+            //Console.WriteLine();
+        }
+
+        public static void CreateNewProduct(XmlDocument xmlDocument)
+        {
+            XmlElement Product = xmlDocument.CreateElement("Product");
+            XmlElement Title = xmlDocument.CreateElement("Title");
+            XmlElement Price = xmlDocument.CreateElement("Price");
+            XmlElement Discount = xmlDocument.CreateElement("Discount");
+
+            Console.Write("Title for new product: ");
+            Title.AppendChild(xmlDocument.CreateTextNode(Console.ReadLine()));
+            Product.AppendChild(Title);
+
+            Console.Write("Price for new product: ");
+            Price.AppendChild(xmlDocument.CreateTextNode(Console.ReadLine()));
+            Product.AppendChild(Price);
+
+            Console.Write("Discount for new product: ");
+            Discount.AppendChild(xmlDocument.CreateTextNode(Console.ReadLine()));
+            Product.AppendChild(Discount);
+
+            xmlDocument.DocumentElement.AppendChild(Product);
+            xmlDocument.Save("Products.xml");
+        }
+    }
+}
